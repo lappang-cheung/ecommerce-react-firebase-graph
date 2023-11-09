2202cf88ada7c20148a5df9176c0680959389184
Leo Cheung <lappangcheung@gmail.com>
2023-11-09 00:50:01 -0500

    Setting up the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import Header from './components/Header/index.jsx'
-import HomePage from './pages/HomePage/index.jsx'
+import { Route, Routes } from 'react-router-dom'
+
+import Header from './components/Header'
+import HomePage from './pages/HomePage'
+import Registration from './pages/Registration'
 
 import './default.scss'
 
@@ -9,7 +12,10 @@ const App = () => {
     <div className="App">
       <Header />
       <div className="main">
-        <HomePage />
+        <Routes>
+          <Route path="/" Component={HomePage} exact/>
+          <Route path="/registration" Component={Registration} />
+        </Routes>
       </div>
     </div>
   )
---
+++ src/pages/Registration/index.jsx
@@ -0,0 +1,14 @@
+import { Component} from 'react'
+import './styles.scss'
+
+class Registration extends Component {
+  render() {
+    return (
+      <div className="registration">
+        <h1>Registration</h1>
+      </div>
+    )
+  }
+}
+
+export default Registration
