2be124d08861511bd1e37298689a2efe7e2bb981
Leo Cheung <lappangcheung@gmail.com>
2023-11-12 14:52:11 -0500

    Setting up the login

--- src/Firebase/utils.js
+++
@@ -1,6 +0,0 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
-
-import {firebaseConfig} from './config'
-
--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,13 +1,36 @@
-import './styles.scss'
+import { Component } from 'react'
 
-const SignIn = props => {
-  return (
-    <div className="signIn">
-      <div className="wrap">
-        <h2>LogIn</h2>
+import Buttons from '../Forms/Button/'
+
+import './styles.scss'
+import {signInWithGoogle} from '../../firebase/utils'
+
+class SignIn extends Component {
+
+  handleSubmit = async e => {
+    e.preventDefault()
+  }
+
+  render() {
+    return (
+      <div className="signIn">
+        <div className="wrap">
+          <h2>LogIn</h2>
+          <div className="formWarp">
+            <form onSubmit={this.handleSubmit}>
+              <div className="socialSignin">
+                <div className="row">
+                  <Buttons onClick={signInWithGoogle}>
+                    Sign In With Google
+                  </Buttons>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default SignIn
